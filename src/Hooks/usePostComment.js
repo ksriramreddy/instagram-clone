c4575be5b25fbd88ae4687a34fd7a5473922d078
ksriramreddy <k.sriramreddy9@gmail.com>
2024-07-23 22:38:02 +0530

    commiting

---
+++ src/Hooks/usePostComment.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useState } from 'react'
+import { useSelector } from 'react-redux'
+
+function usePostComment() {
+    const demo = useSelector(state=>state.user)
+    const authUser = JSON.parse(demo)
+    const [isCommenting,setIsCommenting] = useState(false)
+    if(!authUser){
+        window.alert("Please Login to Comment")
+        return
+    }
+    try {
+        setIsCommenting(true)
+        
+        
+    } catch (error) {
+        
+    }
+    const postComment = (postId,comment)=>{
+        const comment ={
+            comment : comment,
+            createdAt : Date.now(),
+            createdBy : authUser.userId,
+            postId : postId
+        }
+    }
+  return (
+1
+)
+}
+
+export default usePostComment
